0af9c084b31ff7dbfa038c5a3163c23194517eef
Steffen Zschaler <szschaler@acm.org>
2015-11-05 19:35:47 +0000

    Basic implementation of ConfigurationInterpreter framework. Now need to figure out what I actually need to do for configuration statements.

--- uk.ac.kcl.inf.robotics.rigid_bodies/src/uk/ac/kcl/inf/robotics/generator/ConfigurationInterpreter.xtend
+++ uk.ac.kcl.inf.robotics.rigid_bodies/src/uk/ac/kcl/inf/robotics/generator/ConfigurationInterpreter.xtend
@@ -1,22 +1,46 @@
 package uk.ac.kcl.inf.robotics.generator
 
 import java.util.List
+import org.eclipse.emf.ecore.util.EcoreUtil
 import uk.ac.kcl.inf.robotics.rigidBodies.Configuration
+import uk.ac.kcl.inf.robotics.rigidBodies.ConfigurationStatement
+import uk.ac.kcl.inf.robotics.rigidBodies.Joint
+import uk.ac.kcl.inf.robotics.rigidBodies.LockStatement
 import uk.ac.kcl.inf.robotics.rigidBodies.System
 
 class ConfigurationInterpreter {
 	
 	val Configuration config
-	val List<Pair<String, System>> systemInstances
+	val EcoreUtil.Copier copier
 	
 	new (Configuration config, List<Pair<String, System>> systemInstances) {
 		this.config = config
-		this.systemInstances = systemInstances
+
+		// Make a copy of the systems so that we can modify them as needed
+		copier = new EcoreUtil.Copier()
+		systemInstances.forEach[p | 
+			copier.copy(p.value)
+		]
+		copier.copyReferences();
 		
-		// TODO Actually interpret configuration information
+		configureSystems (copier)		
 	}
 	
 	def getConfiguredSystem() {
-		systemInstances.head.value
+		copier.values.head as System
+	}
+	
+	private def configureSystems (EcoreUtil.Copier copier) {
+		config.statements.forEach[stmt |
+			stmt.doConfigure (copier)
+		]
+	}
+	
+	private dispatch def doConfigure (ConfigurationStatement ls, EcoreUtil.Copier copier) {}
+	
+	private dispatch def doConfigure (LockStatement ls, EcoreUtil.Copier copier) {
+		val Joint jointToModify = copier.get (ls.joint) as Joint 
+		
+		// TODO How do I actually express a completely locked joint? What's the right joint type?
 	}
 }
--- uk.ac.kcl.inf.robotics.rigid_bodies/xtend-gen/uk/ac/kcl/inf/robotics/generator/ConfigurationInterpreter.java
+++ uk.ac.kcl.inf.robotics.rigid_bodies/xtend-gen/uk/ac/kcl/inf/robotics/generator/ConfigurationInterpreter.java
@@ -1,23 +1,77 @@
 package uk.ac.kcl.inf.robotics.generator;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Consumer;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.Pair;
 import uk.ac.kcl.inf.robotics.rigidBodies.Configuration;
+import uk.ac.kcl.inf.robotics.rigidBodies.ConfigurationStatement;
+import uk.ac.kcl.inf.robotics.rigidBodies.Joint;
+import uk.ac.kcl.inf.robotics.rigidBodies.LockStatement;
 
 @SuppressWarnings("all")
 public class ConfigurationInterpreter {
   private final Configuration config;
   
-  private final List<Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System>> systemInstances;
+  private final EcoreUtil.Copier copier;
   
   public ConfigurationInterpreter(final Configuration config, final List<Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System>> systemInstances) {
     this.config = config;
-    this.systemInstances = systemInstances;
+    EcoreUtil.Copier _copier = new EcoreUtil.Copier();
+    this.copier = _copier;
+    final Consumer<Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System>> _function = new Consumer<Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System>>() {
+      @Override
+      public void accept(final Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System> p) {
+        uk.ac.kcl.inf.robotics.rigidBodies.System _value = p.getValue();
+        ConfigurationInterpreter.this.copier.copy(_value);
+      }
+    };
+    systemInstances.forEach(_function);
+    this.copier.copyReferences();
+    this.configureSystems(this.copier);
   }
   
   public uk.ac.kcl.inf.robotics.rigidBodies.System getConfiguredSystem() {
-    Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System> _head = IterableExtensions.<Pair<String, uk.ac.kcl.inf.robotics.rigidBodies.System>>head(this.systemInstances);
-    return _head.getValue();
+    Collection<EObject> _values = this.copier.values();
+    EObject _head = IterableExtensions.<EObject>head(_values);
+    return ((uk.ac.kcl.inf.robotics.rigidBodies.System) _head);
+  }
+  
+  private void configureSystems(final EcoreUtil.Copier copier) {
+    EList<ConfigurationStatement> _statements = this.config.getStatements();
+    final Consumer<ConfigurationStatement> _function = new Consumer<ConfigurationStatement>() {
+      @Override
+      public void accept(final ConfigurationStatement stmt) {
+        ConfigurationInterpreter.this.doConfigure(stmt, copier);
+      }
+    };
+    _statements.forEach(_function);
+  }
+  
+  private Object _doConfigure(final ConfigurationStatement ls, final EcoreUtil.Copier copier) {
+    return null;
+  }
+  
+  private Object _doConfigure(final LockStatement ls, final EcoreUtil.Copier copier) {
+    Joint _joint = ls.getJoint();
+    EObject _get = copier.get(_joint);
+    final Joint jointToModify = ((Joint) _get);
+    return null;
+  }
+  
+  private Object doConfigure(final ConfigurationStatement ls, final EcoreUtil.Copier copier) {
+    if (ls instanceof LockStatement) {
+      return _doConfigure((LockStatement)ls, copier);
+    } else if (ls != null) {
+      return _doConfigure(ls, copier);
+    } else {
+      throw new IllegalArgumentException("Unhandled parameter types: " +
+        Arrays.<Object>asList(ls, copier).toString());
+    }
   }
 }
